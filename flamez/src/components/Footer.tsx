e2ec8df83645e38e4c0757793a16d92cdd10ee44
Povindu Samarasekara <povinduchanmith@gmail.com>
2024-07-02 17:37:15 +0530

    refactor: Nav Bar, Footer Changes

--- flamez/src/components/Footer.tsx
+++ flamez/src/components/Footer.tsx
@@ -1,42 +1,58 @@
-import Insta from "../assets/Instagram.png";
-// import Facebook from "../assets/Facebook.png";
-import Twitter from "../assets/Twitter.png";
-import Youtube from "../assets/Youtube.png";
+import Insta from "../assets/SocialIcons/Instagram.png";
+import Facebook from "../assets/SocialIcons/Facebook.png";
+import Twitter from "../assets/SocialIcons/icons8-twitter-squared-50.png";
+import FlamezLogo from "../assets/flamezLogo.png";
 
 export default function footer() {
   return (
-      <div className="bg-black text-white text-sm h-80 pt-12">
-        <div className="grid grid-cols-4 text-center align-middle mb-10">
-          <div className="pt-12 text-lg">
-            <p className="w-full m-auto">
-            <span className="text-amber-500">Flamez</span> Events & <br/> Entertainment
-            </p>
-          </div>
-          <div className="pt-12">
-            Lorem ipsum dolor amet, consectetur adipiscing elit. Eget nisl nunc
-            quam ac sed turpis volutpat. Cursus sed massa non nisi, placerat.
-            (Slogan)
-          </div>
-          <div className="pt-6">
-            <h3 className="font-semibold mb-3">Helpful Links</h3>
-            <ul>
-              <li >Link 1</li>
-              <li className="pt-1">Link 2</li>
-              <li className="pt-1">Link 3</li>
-            </ul>
-          </div>
-          <div className="pt-6 text-left">
-            <h3 className="font-semibold mb-3">Reach us</h3>
-            <ul className="">
-              <li>flamezevents@gmail.com</li>
-              <li className="pt-2">071 013 3491 | 077 459 8115 <br/> 076 123 6357</li>
-              <li className="pt-2">Pepiliyana Road, Boralesgamuwa.</li>
-            </ul>
-          </div>
+    <div className="bg-zinc-900 text-white text-sm h-72 pt-12 font-poppins">
+      <div className="grid grid-cols-4 text-center align-middle mb-10">
+        <div className="pt-12 text-lg">
+          <img src={FlamezLogo} alt="Flamez Logo" className="w-28 h-fit m-auto" />
+          <p className="w-full m-auto leading-5">
+            <span className="text-amber-500">Flamez</span> Events & <br />
+            Entertainment
+          </p>
+          
         </div>
-        <div className="text-center text-xs pb-4">
-          © 2024 Flamez Events. All rights reserved
+        <div className="pt-12">
+          Lorem ipsum dolor amet, consectetur adipiscing elit. Eget nisl nunc
+          quam ac sed turpis volutpat. Cursus sed massa non nisi, placerat.
+          (Slogan)
+        </div>
+        <div className="pt-6">
+          <h3 className="font-semibold mb-3">Helpful Links</h3>
+          <ul>
+            <li>Link 1</li>
+            <li className="pt-1">Link 2</li>
+            <li className="pt-1">Link 3</li>
+          </ul>
+        </div>
+        <div className="pt-6">
+          <h3 className="font-semibold mb-3">Reach us</h3>
+          <ul className="">
+            <li>flamezevents@gmail.com</li>
+            <li className="pt-0">
+              071 013 3491 | 077 459 8115 <br /> 076 123 6357
+            </li>
+            <li className="pt-0">Pepiliyana Road, Boralesgamuwa.</li>
+          </ul>
+          <ul className="flex flex-row gap-1 w-full m-auto justify-center mt-4">
+            <li className="w-6 h-auto">
+              <img src={Insta}></img>
+            </li>
+            <li className="w-6 h-auto">
+              <img src={Facebook}></img>
+            </li>
+            <li className="w-6 h-auto">
+              <img src={Twitter}></img>
+            </li>
+          </ul>
         </div>
       </div>
+      <div className="text-center text-xs pb-2">
+        © 2024 Flamez Events. All rights reserved
+      </div>
+    </div>
   );
 }
--- flamez/src/components/NavbarSimple.tsx
+++ flamez/src/components/NavbarSimple.tsx
@@ -8,9 +8,11 @@ import {
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import FlamezLogo from "../assets/flamezLogo.png";
 
+import { Link } from "react-router-dom";
+
 function NavList() {
   return (
-    <ul className="my-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+    <ul className="my-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 font-poppins">
       <Typography
         as="li"
         variant="small"
@@ -20,12 +22,12 @@ function NavList() {
         onPointerEnterCapture={undefined}
         onPointerLeaveCapture={undefined}
       >
-        <a
-          href="#"
+        <Link
+          to="/"
           className="flex items-center text-black hover:text-amber-500 transition-colors"
         >
           Home
-        </a>
+        </Link>
       </Typography>
       <Typography
         as="li"
@@ -46,12 +48,12 @@ function NavList() {
         color="blue-gray"
         className="p-1 font-medium"
       >
-        <a
-          href="#"
+        <Link
+          to="/gallery"
           className="flex items-center text-black hover:text-amber-500 transition-colors"
         >
           Gallery
-        </a>
+        </Link>
       </Typography>
       <Typography
         as="li"

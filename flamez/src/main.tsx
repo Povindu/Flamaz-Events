e6e779aaf68d89fd3944a2203646921b4effc0ad
Povindu Samarasekara <povinduchanmith@gmail.com>
2024-08-03 14:31:34 +0530

    fix: .env fix

--- flamez/src/main.tsx
+++ flamez/src/main.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { UserProvider } from "./context/authContext";
-
-
 import RoutesPath from "./routes/route";
 
 

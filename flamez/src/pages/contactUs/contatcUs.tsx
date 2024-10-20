343ceef0b111641b43bbec7e9f060d008756b233
Povindu Samarasekara <povinduchanmith@gmail.com>
2024-10-20 12:50:17 +0530

    fix: contact us responsive fix

--- flamez/src/pages/contactUs/contatcUs.tsx
+++ flamez/src/pages/contactUs/contatcUs.tsx
@@ -9,22 +9,22 @@ export default function contatcUs() {
     <div>
       <NavbarSimple />
       <div
-        className="flex flex-row mt-8 justify-center items-center"
+        className="flex flex-col lg:flex-row mt-8 justify-center items-center"
       >
-        <div className=" w-[50%]">
-          <img src={logo} alt="logo" className="w-[60%] m-auto p-20" />
+        <div className="lg:w-[50%]">
+          <img src={logo} alt="logo" className="w-[60%] m-auto p-8 lg:p-20" />
         </div>
-        <div className="w-[50%] p-20">
+        <div className="lg:w-[50%] p-20">
           <p className="text-xl text-center">
             Relax and enjoy your special moments while we take care of all the
             details. Our promise is to make event planning stress-free and
             enjoyable for you.
           </p>
 
-          <div className="p-10 mt-20">
+          <div className="p-4 lg:p-10 mt-20 mb-8">
             <p className="mb-8 text-2xl text-amber-500">Get in touch</p>
-            <div className="flex flex-row">
-              <div className="w-[50%]">
+            <div className="flex flex-col lg:flex-row">
+              <div className="mb-8 lg:mb-0 w-[50%]">
                 <FaLocationDot className="mb-2" />
                 <p>Pepiliyana Road, Boralesgamuwa.</p>
               </div>
@@ -34,8 +34,8 @@ export default function contatcUs() {
               </div>
             </div>
           </div>
-          <div className="p-10">
-            <p className="text-xl mb-2 text-amber-500">
+          <div className="p-4 lg:p-10">
+            <p className="text-xl mb-4 text-amber-500">
               We are here to talk about your plans.{" "}
             </p>
             <p>071 013 3491  |  077 459 8115  |  076 123 6357</p>
